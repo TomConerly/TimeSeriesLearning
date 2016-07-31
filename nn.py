e779dc47fa414977c7b4b2b65c7287186597b378
Tom Conerly <tconerly@fb.com>
2016-07-31 09:38:14 -0700

    layer sizes and learning rate

--- nn.py
+++ nn.py
@@ -13,10 +13,9 @@ def shuffleParallel(L):
         np.random.shuffle(l)
 
 class Input:
-    def __init__(self, fileName, shuffle, normalizeFrom=None):
+    def __init__(self, fileName, shuffle, normalizeFrom=None, ordinalNan=False):
         data = pd.read_csv(fileName)
         ordinal = data[["TIMEVAR1","TIMEVAR2","COVAR_CONTINUOUS_1","COVAR_CONTINUOUS_2","COVAR_CONTINUOUS_3","COVAR_CONTINUOUS_4","COVAR_CONTINUOUS_5","COVAR_CONTINUOUS_6","COVAR_CONTINUOUS_7","COVAR_CONTINUOUS_8","COVAR_CONTINUOUS_9","COVAR_CONTINUOUS_10","COVAR_CONTINUOUS_11","COVAR_CONTINUOUS_12","COVAR_CONTINUOUS_13","COVAR_CONTINUOUS_14","COVAR_CONTINUOUS_15","COVAR_CONTINUOUS_16","COVAR_CONTINUOUS_17","COVAR_CONTINUOUS_18","COVAR_CONTINUOUS_19","COVAR_CONTINUOUS_20","COVAR_CONTINUOUS_21","COVAR_CONTINUOUS_22","COVAR_CONTINUOUS_23","COVAR_CONTINUOUS_24","COVAR_CONTINUOUS_25","COVAR_CONTINUOUS_26","COVAR_CONTINUOUS_27","COVAR_CONTINUOUS_28","COVAR_CONTINUOUS_29","COVAR_CONTINUOUS_30","COVAR_ORDINAL_1","COVAR_ORDINAL_2","COVAR_ORDINAL_3","COVAR_ORDINAL_4","COVAR_ORDINAL_5","COVAR_ORDINAL_6","COVAR_ORDINAL_7","COVAR_ORDINAL_8"]]
-        ordinal = ordinal.fillna(0)
         categoricalOneHot = pd.get_dummies(data, columns=["STUDYID","SITEID","COUNTRY","COVAR_NOMINAL_1","COVAR_NOMINAL_2","COVAR_NOMINAL_3","COVAR_NOMINAL_4","COVAR_NOMINAL_5","COVAR_NOMINAL_6","COVAR_NOMINAL_7","COVAR_NOMINAL_8"], dummy_na=True)
         categoricalEmbedding = data[["SUBJID"]]
         categoricalEmbedding = categoricalEmbedding.apply(lambda x: x.astype('category').cat.codes)
@@ -26,7 +25,9 @@ class Input:
         outputs = outputs.fillna(0)
         outputsPresent = data[["COVAR_y1_MISSING","COVAR_y2_MISSING","COVAR_y3_MISSING"]].astype(int)
 
-        self.npOrdinal = np.array(ordinal).astype(np.float32)
+        self.npOrdinal = np.array(ordinal.fillna(0)).astype(np.float32)
+        if ordinalNan:
+            self.npOrdinal = np.hstack(self.npOrdinal, np.array(1 - ordinal.notnull().astype(np.float32)))
         self.npCategoricalOneHot = np.array(ordinal).astype(np.float32)
         self.npCategoricalEmbedding = np.array(categoricalEmbedding).astype(np.int32)
         self.npOutputs = np.array(outputs).astype(np.float32)
@@ -37,28 +38,39 @@ class Input:
             shuffleParallel([self.npOrdinal, self.npCategoricalOneHot, self.npCategoricalEmbedding, self.npOutputs, self.npOutputsPresent])
 
         if normalizeFrom is not None:
-            inputNormFrom = self if fileName == normalizeFrom else Input(normalizeFrom, shuffle=False)
+            inputNormFrom = self if fileName == normalizeFrom else Input(normalizeFrom, shuffle=False, ordinalNan=ordinalNan)
             means = inputNormFrom.npOrdinal.mean(axis=0)
             std = inputNormFrom.npOrdinal.std(axis=0)
             self.npOrdinal = (self.npOrdinal - means) / std
 
+    def roll(shift):
+        self.npOrdinal = np.roll(self.npOrdinal, shift, axis=0)
+        self.npCategoricalOneHot = np.roll(self.npCategoricalOneHot, shift, axis=0)
+        self.npCategoricalEmbedding = np.roll(self.npCategoricalEmbedding, shift, axis=0)
+        self.npOutputs = np.roll(self.npOutputs, shift, axis=0)
+        self.npOutputsPresent = np.roll(self.npOutputsPresent, shift, axis=0)
+
 class Settings:
-    def __init__(self, runId, resumeRun=None, firstLayerSize=100, secondLayerSize=50, batchSize=100, trainingTime=60*60, dropout=1.0, learningRate=1e-4, trainingPercent=0.8, normalizeInput=False):
+    def __init__(self, runId, resumeRun, hiddenLayerSizes, batchSize=100, trainingTime=60*60, dropout=1.0, learningRate0=1e-4, learningRate1=1e-4, learningRatet=100000, trainingPercent=0.8, normalizeInput=False, validationOffset=0.8, ordinalNan=False):
         self.runId = runId
         self.resumeRun = resumeRun
-        self.firstLayerSize = firstLayerSize
-        self.secondLayerSize = secondLayerSize
+        self.hiddenLayerSizes = hiddenLayerSizes
         self.batchSize = batchSize
         self.trainingTime = trainingTime
         self.dropout = dropout
         self.learningRate = learningRate
         self.trainingPercent = trainingPercent
         self.normalizeInput = normalizeInput
+        self.validationOffset = validationOffset
+        self.ordinalNan = ordinalNan
+        self.learningRate0 = learningRate0
+        self.learningRate1 = learningRate1
+        self.learningRatet = learningRatet
 
     def compatible(self, s):
-        if self.firstLayerSize != s.firstLayerSize:
+        if self.hiddenLayerSizes != s.hiddenLayerSizes:
             return False
-        if self.secondLayerSize != s.secondLayerSize:
+        if self.ordinalNan != s.ordinalNan:
             return False
         return True
 
@@ -69,53 +81,57 @@ class Graph:
         self.categoricalOneHotInputs = tf.placeholder(tf.float32, [None, categoricalOneHotInputSize], name='categoricalOneHotInputs')
         self.outputs = tf.placeholder(tf.float32, [None, 3], name='outputs')
         self.outputsPresent = tf.placeholder(tf.float32, [None, 3], name='outputsPresent')
+        self.learningRate = tf.placeholder(tf.float32, [1], name='outputsPresent')
 
-        w11 = tf.Variable(tf.truncated_normal([ordinalInputSize, settings.firstLayerSize], stddev=0.1), name="w11")
-        w12 = tf.Variable(tf.truncated_normal([categoricalOneHotInputSize, settings.firstLayerSize], stddev=0.1), name="w12")
-        b1 = tf.Variable(tf.constant(0.1, shape=[settings.firstLayerSize]), name="b1")
+        w11 = tf.Variable(tf.truncated_normal([ordinalInputSize, settings.hiddenLayerSizes[0]], stddev=0.1), name="w11")
+        w12 = tf.Variable(tf.truncated_normal([categoricalOneHotInputSize, settings.hiddenLayerSizes[0]], stddev=0.1), name="w12")
+        b1 = tf.Variable(tf.constant(0.1, shape=[settings.hiddenLayerSizes[0]]), name="b1")
         h1 = tf.matmul(self.ordinalInputs, w11) + tf.matmul(self.categoricalOneHotInputs, w12) + b1
         self.categoricalFeatureEmbedInputs = []
         for numClasses, embedSize in categoricalFeatureEmbedSizes:
             embedWeights = tf.Variable(tf.truncated_normal([numClasses, embedSize], stddev=0.1))
             embedInput = tf.placeholder(tf.int32, shape=[None])
             embedOutput = tf.nn.embedding_lookup(embedWeights, embedInput)
-            firstLayerWeights = tf.Variable(tf.truncated_normal([embedSize, settings.firstLayerSize], stddev=0.1))
+            firstLayerWeights = tf.Variable(tf.truncated_normal([embedSize, settings.hiddenLayerSizes[0]], stddev=0.1))
             h1 = h1 + tf.matmul(embedOutput, firstLayerWeights)
             self.categoricalFeatureEmbedInputs.append(embedInput)
-
         z1 = tf.nn.relu(h1)
         z1drop = tf.nn.dropout(z1, self.keep_prob)
 
-        w2 = tf.Variable(tf.truncated_normal([settings.firstLayerSize, settings.secondLayerSize], stddev=0.1), name="w2")
-        b2 = tf.Variable(tf.constant(0.1, shape=[settings.secondLayerSize]), name="b2")
-        h2 = tf.matmul(z1drop, w2) + b2
-        z2 = tf.nn.relu(h2)
-        z2drop = tf.nn.dropout(z2, self.keep_prob)
+        zdrops = [z1drop]
+        for i in range(1, len(settings.hiddenLayerSizes)):
+            w = tf.Variable(tf.truncated_normal([settings.hiddenLayerSizes[i-1], settings.hiddenLayerSizes[i]], stddev=0.1))
+            b = tf.Variable(tf.constant(0.1, shape=[settings.hiddenLayerSizes[i]]))
+            h = tf.matmul(zdrops[-1], w) + b
+            z = tf.nn.relu(h)
+            zdrop = tf.nn.dropout(z, self.keep_prob)
+            zdrops.append(zdrop)
 
-        w3 = tf.Variable(tf.truncated_normal([settings.secondLayerSize, 3], stddev=0.1), name="w3")
-        b3 = tf.Variable(tf.constant(0.1, shape=[3]), name="b3")
-        self.h3 = tf.matmul(z2drop, w3) + b3
+        woutput = tf.Variable(tf.truncated_normal([settings.hiddenLayerSizes[-1], 3], stddev=0.1), name="w3")
+        boutput = tf.Variable(tf.constant(0.1, shape=[3]), name="b3")
+        self.houtput = tf.matmul(zdrops[-1], woutput) + boutput
 
-        self.mse = tf.reduce_mean(tf.mul(tf.square(self.h3 - self.outputs), self.outputsPresent), name='mse')
-        self.mad = tf.reduce_mean(tf.mul(tf.abs(self.h3 - self.outputs), self.outputsPresent) , name='mad')
-        self.train_step = tf.train.AdamOptimizer(learning_rate=settings.learningRate).minimize(self.mad)
+        self.mse = tf.reduce_mean(tf.mul(tf.square(self.houtput - self.outputs), self.outputsPresent), name='mse')
+        self.mad = tf.reduce_mean(tf.mul(tf.abs(self.houtput - self.outputs), self.outputsPresent) , name='mad')
+        self.train_step = tf.train.AdamOptimizer(learning_rate=self.learningRate).minimize(self.mad)
 
         tf.scalar_summary('mse', self.mse)
         tf.scalar_summary('mad', self.mad)
         self.summary_op = tf.merge_all_summaries()
 
-def makeFeedDict(graph, input, start=None, end=None, keep_prob=1.0):
+def makeFeedDict(graph, input, start=None, end=None, keep_prob=1.0, learningRate=0.0):
     feed_dict = {graph.ordinalInputs: input.npOrdinal[start:end],
                  graph.categoricalOneHotInputs: input.npCategoricalOneHot[start:end],
                  graph.outputs: input.npOutputs[start:end],
                  graph.outputsPresent: input.npOutputsPresent[start:end],
-                 graph.keep_prob: keep_prob}
+                 graph.keep_prob: keep_prob,
+                 graph.learningRate: learningRate}
     for i in range(len(graph.categoricalFeatureEmbedInputs)):
         feed_dict[graph.categoricalFeatureEmbedInputs[i]] = input.npCategoricalEmbedding[start:end,i]
     return feed_dict
 
 def predict(settings):
-    testInput = Input('testData.csv', shuffle=False, normalizeFrom='training.csv' if settings.normalizeInput else None)
+    testInput = Input('testData.csv', shuffle=False, normalizeFrom='training.csv' if settings.normalizeInput else None, ordinalNan=settings.ordinalNan)
     categoricalFeatureEmbedSizes = zip([x + 1 for x in testInput.npCategoricalEmbedding.max(axis=0)], [10] * testInput.npCategoricalEmbedding.shape[1])
     graph = Graph(settings, testInput.npOrdinal.shape[1], testInput.npCategoricalOneHot.shape[1], categoricalFeatureEmbedSizes)
 
@@ -123,12 +139,15 @@ def predict(settings):
     saver = tf.train.Saver()
     saver.restore(sess, os.path.join('tfmodels', 'run{}'.format(settings.runId)))
 
-    testPredictions = sess.run(graph.h3, feed_dict=makeFeedDict(graph, testInput))
+    testPredictions = sess.run(graph.houtput, feed_dict=makeFeedDict(graph, testInput))
     np.savetxt('pred.csv', testPredictions, delimiter=',', fmt='%.9f')
 
 def nn(settings):
-    trainInput = Input('training.csv', shuffle=True, normalizeFrom='training.csv' if settings.normalizeInput else None)
-    trainValidationBoundary = int(trainInput.npOutputs.shape[0] * settings.trainingPercent)
+    trainInput = Input('training.csv', shuffle=True, normalizeFrom='training.csv' if settings.normalizeInput else None, ordinalNan=settings.ordinalNan)
+    trainingSize = int(trainInput.npOutputs.shape[0] * settings.trainingPercent)
+    validationStart = int(trainInput.npOutputs.shape[0] * settings.validationOffset)
+    trainInput.roll(trainingSize - validationStart)
+
     categoricalFeatureEmbedSizes = zip([x + 1 for x in trainInput.npCategoricalEmbedding.max(axis=0)], [10] * trainInput.npCategoricalEmbedding.shape[1])
     graph = Graph(settings, trainInput.npOrdinal.shape[1], trainInput.npCategoricalOneHot.shape[1], categoricalFeatureEmbedSizes)
 
@@ -152,7 +171,12 @@ def nn(settings):
         start = at * settings.batchSize
         end = min(start + settings.batchSize, trainValidationBoundary)
         at += 1
-        sess.run(graph.train_step, feed_dict=makeFeedDict(graph, trainInput, start=start, end=end, keep_prob=settings.dropout))
+        if step >= settings.learningRatet:
+            learningRate = settings.learningRate1
+        else:
+            alpha = step / settings.learningRatet
+            learningRate = (1 - alpha) * settings.learningRate0 + alpha * settings.learningRate1
+        sess.run(graph.train_step, feed_dict=makeFeedDict(graph, trainInput, start=start, end=end, keep_prob=settings.dropout, learningRate=learningRate))
 
         if step % 100 == 0:
             summary_train_writer.add_summary(sess.run(graph.summary_op, feed_dict=makeFeedDict(graph, trainInput, end=trainValidationBoundary)), step)
@@ -175,17 +199,21 @@ def main():
     parser.add_argument('--resumeRun', default=None, type=int, help='id of the run')
     parser.add_argument('--train', action='store_true', default=False, help='')
     parser.add_argument('--predict', action='store_true', default=False, help='')
-    parser.add_argument('--firstLayerSize', type=int, default=2000, help='firstLayerSize')
-    parser.add_argument('--secondLayerSize', type=int, default=1000, help='secondLayerSize')
+    parser.add_argument('--hiddenLayerSizes', nargs='+', type=int)
     parser.add_argument('--batchSize', type=int, default=1000, help='batchSize')
     parser.add_argument('--trainingTime', type=int, default=60*60*5, help='trainingTime')
     parser.add_argument('--dropout', type=float, default=1.0, help='dropout')
-    parser.add_argument('--learningRate', type=float, default=1e-3, help='learningRate')
+    parser.add_argument('--learningRate0', type=float, default=1e-3, help='')
+    parser.add_argument('--learningRate1', type=float, default=1e-4, help='')
+    parser.add_argument('--learningRatet', type=float, default=100000, help='')
     parser.add_argument('--normalizeInput', action='store_true', default=False, help='')
+    parser.add_argument('--trainingPercent', type=float, default=0.8, help='trainingPercent')
+    parser.add_argument('--validationOffset', type=float, default=0.8, help='validationOffset')
+    parser.add_argument('--ordinalNan', action='store_true', default=False, help='')
     args = parser.parse_args()
 
     if args.train:
-        settings = Settings(args.runId, args.resumeRun, firstLayerSize=args.firstLayerSize, secondLayerSize=args.secondLayerSize, batchSize=args.batchSize, trainingTime=args.trainingTime, dropout=args.dropout, learningRate=args.learningRate, normalizeInput = args.normalizeInput)
+        settings = Settings(args.runId, args.resumeRun, args.hiddenLayerSizes, batchSize=args.batchSize, trainingTime=args.trainingTime, dropout=args.dropout, learningRate0=args.learningRate0, learningRate1=args.learningRate1, learningRatet=args.learningRatet, normalizeInput=args.normalizeInput, trainingPercent=args.trainingPercent, validationOffset=args.validationOffset, ordinalNan=args.ordinalNan)
         if os.path.isfile(getSettingsPath(args.runId)):
             logging.info('Run already exists! Exiting')
             return
