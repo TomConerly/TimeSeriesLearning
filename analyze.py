d521881929ae7c00c807465abf7bca2088528c01
Tom Conerly <tconerly@fb.com>
2016-08-09 13:02:56 -0700

    add combined for ordinals

--- analyze.py
+++ analyze.py
@@ -1,3 +1,4 @@
+import itertools
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -21,9 +22,9 @@ if printTopN:
         for a in trainVC.axes[0][:10]:
             trainProb = trainVC[a] if a in trainVC else 0
             testProb = testVC[a] if a in testVC else 0
-            if trainProb < .01:
+            if trainProb < .001:
                 break
-            #print('{}: {} {}'.format(a, trainProb, testProb))
+            print('{}: {} {}'.format(a, trainProb, testProb))
         #print()
 
 if printMissingProb:
@@ -34,7 +35,7 @@ if printMissingProb:
 def timeMap(data):
     times = {}
     for row in data.itertuples():
-        key = (row.STUDYID, row.SUBJID) 
+        key = (row.STUDYID, row.SUBJID)
         value = row.TIMEVAR1
         if key not in times:
             times[key] = [value]
@@ -69,13 +70,11 @@ if printTimes:
 def numUniques(l):
     return len(set(l))
 
-x = np.array(train['STUDYID'])
-y = np.array(train['SITEID'])
-z = np.array(train['COUNTRY'])
-print('study', numUniques(x))
-print('site', numUniques(y))
-print('country', numUniques(z))
-print('study/site', numUniques(zip(x, y)))
-print('study/country', numUniques(zip(x, z)))
-print('site/country', numUniques(zip(y, z)))
-print('study/site/country', numUniques(zip(x, y, z)))
+def powerset(iterable):
+    "powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"
+    s = list(iterable)
+    return itertools.chain.from_iterable(itertools.combinations(s, r) for r in range(len(s)+1))
+CATEGORICAL_COLS = ["STUDYID","SITEID","COUNTRY","COVAR_NOMINAL_1","COVAR_NOMINAL_2","COVAR_NOMINAL_3","COVAR_NOMINAL_4","COVAR_NOMINAL_5","COVAR_NOMINAL_6","COVAR_NOMINAL_7","COVAR_NOMINAL_8"]
+for s in powerset(CATEGORICAL_COLS):
+    print(s, len(set(zip(*[np.array(train[col]) for col in s]))), *[len(train[col].unique()) for col in s])
+
