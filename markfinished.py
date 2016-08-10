d3627eecaff55cd2c4dece2bc63c66686d47f130
Tom Conerly <tconerly@fb.com>
2016-08-10 10:38:37 -0700

    fix

--- markfinished.py
+++ markfinished.py
@@ -1,6 +1,7 @@
 import aws
 import command
 import nn
+import pickle
 
 awsClient = aws.RealAWSClient()
 cmdClient = command.CommandClient('52.41.178.184',
@@ -8,5 +9,5 @@ cmdClient = command.CommandClient('52.41.178.184',
 
 workId = 14
 cmdClient.sendFinishedWorkRequest(workId)
-history = [StepScore(trainMAD=1, trainMSE=1, validMAD=1, validMSE=1, step=1)]
+history = [nn.StepScore(trainMAD=1, trainMSE=1, validMAD=1, validMSE=1, step=1)]
 awsClient.putObject(aws.S3BUCKET, '{}{}.result'.format(command.prefix, workId), pickle.dumps(history))
