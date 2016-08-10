889bb4e7b6f0155b23c95737cc542bce3a9e1024
Tom Conerly <tconerly@fb.com>
2016-08-09 18:02:23 -0700

    change how ob is printed

--- nn.py
+++ nn.py
@@ -207,7 +207,7 @@ class Settings:
             if col.startswith('COVAR_NOMINAL_'):
                 name = 'cvn' + col[-1]
             cat += '{}:{},'.format(name.lower(), getattr(self, col))
-        return 'Run: {}, Res: {}. Graph[Hid: {}, Norm: {}, OrdNan: {}, Cat: {}, Act: {}, BN: {}, CN: {:.5f}, IB: {:.5f}, WM: {:.5f}]<br> Training[Batch: {}, Time: {}, Drop: {}, l0: {:.5f}, l1: {:.5f}, lt: {}, train: {}, valOff: {}, l1r: {:.5f}, l2r: {:.5f}, OB: {:.5f}]'.format(self.runId, self.resumeRun, self.hiddenLayerSizes, 'T' if self.normalizeInput else 'F', 'T' if self.ordinalNan else 'F', cat, self.activation, self.batchNorm, self.clipNorm, self.initialBias, self.weightMax, self.batchSize, self.trainingTime, self.dropout, self.learningRate0, self.learningRate1, self.learningRatet, self.trainingPercent, self.validationOffset, self.l1reg, self.l2reg, self.outputBias)
+        return 'Run: {}, Res: {}. Graph[Hid: {}, Norm: {}, OrdNan: {}, Cat: {}, Act: {}, BN: {}, CN: {:.5f}, IB: {:.5f}, WM: {:.5f}]<br> Training[Batch: {}, Time: {}, Drop: {}, l0: {:.5f}, l1: {:.5f}, lt: {}, train: {}, valOff: {}, l1r: {:.5f}, l2r: {:.5f}, OB: {}]'.format(self.runId, self.resumeRun, self.hiddenLayerSizes, 'T' if self.normalizeInput else 'F', 'T' if self.ordinalNan else 'F', cat, self.activation, self.batchNorm, self.clipNorm, self.initialBias, self.weightMax, self.batchSize, self.trainingTime, self.dropout, self.learningRate0, self.learningRate1, self.learningRatet, self.trainingPercent, self.validationOffset, self.l1reg, self.l2reg, self.outputBias)
 
 def batchNorm(inputTensor, useBatchNorm, isTraining, decay=0.99):
     if not useBatchNorm:
